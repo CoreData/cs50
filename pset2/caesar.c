dffa2cf3d2e206e8d64e399f5972d6858eae7a54
Chris Anders <github@hell-labs.de>
2012-11-10 17:06:38 +0100

    Comments inserted.

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -1,9 +1,8 @@
 /*
     Author: Chris Anders, chris@hell-labs.de
     
-    This file presents a solution to pset2 of CS50x.
+    This file presents a solution for the caesar problem in pset2 of CS50x.
 */
-        
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
@@ -11,47 +10,57 @@
                         
 int main(int argc, string argv[])
 {
+    // Variable declarations
     bool keySuccessful = false;
     int key = 0;
     int input_length = 0;
-    // string text = "Be sure to drink your Ovaltine!";
     string text = "";
     
+    // Get input
     do
     {
+        // The number of command line args submitted was incorrect. 
         if(argc != 2)
         {
             printf("You didn't submit a valid encryption key.\n");
             printf("Please check your input and re-run the programm.\n");
+            printf("We require an integer as encryption key.")
             return 1;
         }
         else
         {
+            // Key needs to be converted from string to int
             key = atoi(argv[1]);
             keySuccessful = true;
         }
     } while(!keySuccessful);
     
+    // Read user input
     text = GetString();
     input_length = strlen(text);
     for(int i = 0; i < input_length; i++)
     {
+        // Process characters
         if(isalpha(text[i]))
         {
+            // Work with lower case characters
             if(islower(text[i]))
             {
                 printf("%c", ((((text[i] - 97)+key)%26)+97));
             }
-            else if(isupper(text[i]))
+            // Work upper lower case characters
+            else
             {
                 printf("%c", ((((text[i] - 65)+key)%26)+65));
             }
         }
+        // "Mirror" all other input to the result string w/out processing
         else
         {
             printf("%c", text[i]);
         }
     }
+    // Be a good citizen and exit. 
     printf("\n");
     return 0;  
 }
