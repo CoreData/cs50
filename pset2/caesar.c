8513156f6ca39e02d4b7d8132231779c66db54ba
Chris Anders <github@hell-labs.de>
2012-11-10 10:43:00 +0100

    Intital commit for the solution of the Caesar problem.

---
+++ pset2/caesar.c
@@ -0,0 +1,17 @@
+/*
+    Author: Chris Anders, chris@hell-labs.de
+    
+    This file presents a solution to pset2 of CS50x.
+*/
+        
+#include <stdio.h>
+#include <cs50.h>
+        
+int main(int argc, string argv[])
+{
+    for(int i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+    return 0;  
+}
