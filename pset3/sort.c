1b5e926d40f671e9b84a58eec57ff4a377d3f3f2
Chris Anders <github@hell-labs.de>
2012-11-17 00:51:50 +0100

    Initial commit for sorting algorithm.

---
+++ pset3/sort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define SIZE 8
+
+void sort(int array[], int size)
+{
+    // TODO IMPLEMENT
+}
+
+int main(void)
+{
+    int numbers[SIZE] = { 4, 15, 16, 50, 8, 23, 42, 108 };
+    for (int i = 0; i < SIZE; i++)
+        printf("%d ", numbers[i]);
+    printf("\n");
+    sort(numbers, SIZE);
+    for (int i = 0; i < SIZE; i++)
+        printf("%d ", numbers[i]);
+    printf("\n");
+    return 0;
+}
