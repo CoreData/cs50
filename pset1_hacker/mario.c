56c2cdd28cd377d4d045065feb27a5150fdce746
Chris Anders <github@hell-labs.de>
2012-11-16 16:33:07 +0100

    Possible solution for the mario problem in pset1 Hacker edition

---
+++ pset1_hacker/mario.c
@@ -0,0 +1,55 @@
+/*
+    mario.c
+    
+    Author: Chris Anders, chris@hell-labs.de
+    
+    This file is a solution of the mario pyramid problem from
+    pset1_hacker of CS50.
+*/
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) 
+{
+	// Declare and intialize variables
+	int pyramid_height = 0;
+	
+	// Grab user input, check it and ask for retry if not within bounds  
+	do {
+		printf("Height:");
+		pyramid_height = GetInt();
+        if (pyramid_height == 0)
+        {
+            return 0;
+        }
+	} while (pyramid_height < 1 || pyramid_height > 23);
+
+	/*
+		The outer loop handles the count of rows to print.
+		The inner loops are responsible for printing the correct number of 
+		spaces and hashes. The first of the two inner loop handles spaces
+        It takes the value of the loop counter form the outer loop
+		plus 2 as border value since we starting the pyramid with 2 blocks;
+	*/ 
+	for(int i = 0; i < pyramid_height; i++) 
+	{
+		// Print the required spaces
+        for(int j = 0; j < pyramid_height-i-1; j++)
+        {
+            printf("%s", " ");
+        }
+        // Print the '#' character.
+		for(int k = 0; k < i+1; k++)
+		{
+			printf("#");
+		}
+		printf("  ");
+		for(int k = 0; k < i+1; k++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}	
+	return 0;
+}
