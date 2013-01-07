44c366bcb6b074d5d0f8985a4cdbba91551cb3fc
Chris Anders <github@hell-labs.org>
2013-01-07 10:08:22 +0100

    Somewhat insecure implementation of cp. Needs work as indicated in the comments.

--- pset4/section5/cp.c
+++ pset4/section5/cp.c
@@ -8,7 +8,32 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    // TODO: copy the contents of argv[1] to argv[2]
+    FILE* sfile = fopen(argv[1], "r");
+    FILE* dfile = fopen(argv[2], "w");
+
+    if(sfile == NULL) 
+    {
+    	printf("Problems opening source file!\n");
+    	return 1;
+    }
+    if(dfile == NULL)
+    {
+    	printf("Problems opening destination file!\n");
+    	return 2;
+    }
+
+    // We should handle this in a dynamic fashion. 
+    // Using fgets with a static char array we will f*ck up 
+    // when we have a really long line.
+    char line[1024];
+
+    while(fgets(line, sizeof(line), sfile) != NULL)
+    {
+    	fprintf(dfile, "%s", line);
+    }
+
+    fclose(sfile);
+    fclose(dfile);
 
     return 0;
 }
