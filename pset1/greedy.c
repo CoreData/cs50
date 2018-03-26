0d584c38646f11099ee2828a64337f4f75908514
pfothergill <37722637+pfothergill@users.noreply.github.com>
2018-03-25 20:46:39 -0500

    Update to "Pretty Print"
    I liked your "pretty print" version and I expanded on it to include twenties, tens, fives, and dollar bills!

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -1,36 +1,34 @@
-/*
-    greedy.c
-    
-    Author: Chris Anders, chris@hell-labs.de
-    
-    This file is a solution of the greedy algorithm problem from
-    pset1 of CS50.
-*/
-
 #include <stdio.h>
 #include <cs50.h>
 #include <math.h>
 
-#define QUARTER 25;
-#define DIME 10;
-#define NICKEL 5;
-
-int main(void) 
+int main(void)
 {
     // Variable declarations
     float given_amount = 0;
     int cent_amount = 0;
+    int twenty_count = 0;
+    int ten_count = 0;
+    int five_count = 0;
+    int dollar_count = 0;
     int quarter_count = 0;
     int dime_count = 0;
     int nickel_count = 0;
     int leftover = 0;
     int coin_count = 0;
-    
+    int TWENTY = 2000;
+    int TEN = 1000;
+    int FIVE = 500;
+    int DOLLAR = 100;
+    int QUARTER = 25;
+    int DIME = 10;
+    int NICKEL = 5;
+
     //Input handling
-    do 
+    do
     {
-        printf("You gave me: ");
-        given_amount = GetFloat();
+        printf("You gave me: $");
+        given_amount = get_float();
         //If given amount is zero or less then zero checked
         if(given_amount == 0||given_amount <= 0)
         printf("Number Should be greater then Zero EG:10\n:");
@@ -40,26 +38,42 @@ int main(void)
     // Given amount is convert to cents
     cent_amount = (int)round(given_amount*100);
 
+    // TWENTY'S
+    twenty_count = cent_amount / TWENTY;
+    leftover = cent_amount % TWENTY;
+
+    // TEN'S
+    ten_count = leftover / TEN;
+    leftover = leftover % TEN;
+
+    // FIVE'S
+    five_count = leftover / FIVE;
+    leftover = leftover % FIVE;
+
+    // Dolla Dolla Bills Y'all
+    dollar_count = leftover / DOLLAR;
+    leftover = leftover % DOLLAR;
+
     // Quarters
-    quarter_count = cent_amount / QUARTER;
-    leftover = cent_amount % QUARTER;
-    
+    quarter_count = leftover / QUARTER;
+    leftover = leftover % QUARTER;
+
     // Dimes
     dime_count = leftover / DIME;
     leftover = leftover % DIME;
-    
+
     // Nickels
     nickel_count = leftover / NICKEL;
     leftover = leftover % NICKEL;
-    
+
     // Leftover at this stage is pennies
     coin_count = quarter_count + dime_count + nickel_count + leftover;
-    
+
     // Pretty print
-    // printf("You get %d coins: %d quarters, %d dimes, %d nickels and %d pennies.\n", coin_count, quarter_count, dime_count, nickel_count, leftover);
-    
+    printf("Bills: %d Twentys, %d tens, %d fives, %d dollars -- And %d coins: %d quarters, %d dimes, %d nickels and \n%d pennies.\n", twenty_count, ten_count, five_count, dollar_count, coin_count, quarter_count, dime_count, nickel_count, leftover);
+
     //Required output:
-    printf("%d\n", coin_count);
-    
+    //printf("%d\n", coin_count);
+
     return 0;
 }
